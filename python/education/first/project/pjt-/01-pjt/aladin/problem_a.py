0e1968eeb206f97de23bfcb603b623304bd425d6
wkdtmddus <jangsy1207@gmail.com>
2024-01-24 16:51:48 +0900

    fix python/education/first/project/pjt-/

--- python/education/first/project/pjt-/01-pjt/aladin/problem_a.py
+++ python/education/first/project/pjt-/01-pjt/aladin/problem_a.py
@@ -2,21 +2,17 @@ import json
 from pprint import pprint
 
 
-def artist_info(artist):
+def book_info(book, categories):
+    pass
     # 여기에 코드를 작성합니다.
-    # pprint(artist)
-    result = {
-        'id': artist.get('id'),
-        'name': artist.get('name'),
-        'genres_ids': artist.get('genres_ids'),
-        'images': artist.get('images'),
-        'type': artist.get('type')
-    }
-    return result
+
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
-    artist_json = open('data/artist.json', encoding='utf-8')
-    artist_dict = json.load(artist_json)
+    book_json = open('data/book.json', encoding='utf-8')
+    book = json.load(book_json)
 
-    pprint(artist_info(artist_dict))
+    categories_json = open('data/categories.json', encoding='utf-8')
+    categories_list = json.load(categories_json)
+
+    pprint(book_info(book, categories_list))
--- python/education/first/project/pjt-/01-pjt/spotify/problem_a.py
+++ python/education/first/project/pjt-/01-pjt/spotify/problem_a.py
@@ -4,7 +4,15 @@ from pprint import pprint
 
 def artist_info(artist):
     # 여기에 코드를 작성합니다.
-    pass
+    # pprint(artist)
+    result = {
+        'id': artist.get('id'),
+        'name': artist.get('name'),
+        'genres_ids': artist.get('genres_ids'),
+        'images': artist.get('images'),
+        'type': artist.get('type')
+    }
+    return result
 
 
 # 아래의 코드는 수정하지 않습니다.
