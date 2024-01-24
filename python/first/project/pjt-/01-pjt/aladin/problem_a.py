345a0638b5b613ff4aae8cc218330115243d545f
wkdtmddus <jangsy1207@gmail.com>
2024-01-24 17:18:02 +0900

    fix python/first/project/project/pjt-/01-pjt/aladin/problem_a.py

--- python/first/project/pjt-/01-pjt/aladin/problem_a.py
+++ python/first/project/pjt-/01-pjt/aladin/problem_a.py
@@ -2,7 +2,7 @@ import json
 from pprint import pprint
 
 
-def book_info(book, categories):
+def book_info(book):
     pass
     # 여기에 코드를 작성합니다.
 
@@ -12,7 +12,4 @@ if __name__ == '__main__':
     book_json = open('data/book.json', encoding='utf-8')
     book = json.load(book_json)
 
-    categories_json = open('data/categories.json', encoding='utf-8')
-    categories_list = json.load(categories_json)
-
-    pprint(book_info(book, categories_list))
+    pprint(book_info(book))
