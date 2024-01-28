c4f868990be3d342407e4444f2069daa6c097fc7
wkdtmddus <jangsy1207@gmail.com>
2024-01-28 18:24:36 +0900

    feat python/first/practice/python_4/python_hw_4_4/

---
+++ python/first/practice/python_4/python_hw_4_4/hw_4_4.py
@@ -0,0 +1,39 @@
+list_of_book = [
+    '장화홍련전',
+    '가락국 신화',
+    '온달 설화',
+    '금오신화',
+    '이생규장전',
+    '만복자서포기',
+    '수성지',
+    '백호집',
+    '원생몽유록',
+    '홍길동전',
+    '장생전',
+    '도문대작',
+    '옥루몽',
+    '옥련몽',
+]
+
+rental_book = [
+    '장생전',
+    '위대한 개츠비',
+    '원생몽유록',
+    '이생규장전',
+    '데미안',
+    '장화홍련전',
+    '수성지',
+    '백호집',
+    '난중일기',
+    '홍길동전',
+    '만복자서포기',
+]
+
+
+missing_book = [book for book in rental_book if book not in list_of_book]
+
+if len(missing_book) == 0:
+    print('모든 책을 보유하고 있습니다.')
+else:
+    for book in missing_book:
+        print(f'{book} 을/를 보충하여야 합니다.')
