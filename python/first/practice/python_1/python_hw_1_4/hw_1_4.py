fad55105e6e607bfb7e231c6be3c4b9f244b2d5e
wkdtmddus <jangsy1207@gmail.com>
2024-01-28 13:06:49 +0900

    feat python/first/practice/python_1/python_hw_1_4/

---
+++ python/first/practice/python_1/python_hw_1_4/hw_1_4.py
@@ -0,0 +1,13 @@
+# 사과는 영어로 apple
+# 바나나는 영어로 banana
+# 키위는 영어로 kiwi
+
+apple = 'apple'
+banana = 'banana'
+kiwi = 'kiwi'
+english = '영어로'
+kApple = '사과는'
+kBanana = '바나나는'
+kKiwi = '키위는'
+
+print(f'{kApple} {english} {apple}\n{kBanana} {english} {banana}\n{kKiwi} {english} {kiwi}')
