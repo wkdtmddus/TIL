f7dfec82728f8b57e35d731e7f8d6046778aae96
wkdtmddus <jangsy1207@gmail.com>
2024-01-28 16:12:15 +0900

    feat python/first/practice/python_3/python_ws_3_4/

---
+++ python/first/practice/python_3/python_ws_3_4/ws_3_4.py
@@ -0,0 +1,24 @@
+number_of_people = 0
+
+
+def increase_user():
+    global number_of_people
+    number_of_people += 1
+
+
+def create_user(name, age, address):
+    increase_user()
+    print(f'{name}님 환영합니다!')
+    user_info = {
+        'name' : name,
+        'age' : age,
+        'address' : address
+    }
+    return user_info
+
+
+name = ['김시습', '허균', '남영로', '임제', '박지원']
+age = [20, 16, 52, 36, 60]
+address = ['서울', '강릉', '조선', '나주', '한성부']
+
+print(list(map(create_user, name, age, address)))
