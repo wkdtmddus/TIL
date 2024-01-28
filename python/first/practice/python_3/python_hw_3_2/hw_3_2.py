a7851264dfca4c40f67d0a38e082144435646bf5
wkdtmddus <jangsy1207@gmail.com>
2024-01-28 16:29:23 +0900

    feat python/first/practice/python_3/python_hw_3_2/

---
+++ python/first/practice/python_3/python_hw_3_2/hw_3_2.py
@@ -0,0 +1,11 @@
+# 아래 함수를 수정하시오.
+def add_numbers(a, b):
+    return a + b
+
+
+
+# 수정한 add_numbers() 함수를 호출하시오.
+num1 = 3
+num2 = 5
+
+print(f'{num1}과 {num2}를 인자로 넘긴 경우,\n{add_numbers(num1,num2)}')
