9700c8d8dad3ae63eafee96da88db8c43535ec2c
wkdtmddus <jangsy1207@gmail.com>
2024-02-04 14:30:16 +0900

    feat python/first/test/exam/exam_3/

---
+++ python/first/test/exam/exam_3/exam_3.py
@@ -0,0 +1,38 @@
+############## 주의 ##############
+# 입력을 받기위한 input 함수는 절대 사용하지 않습니다.
+# 내장 함수 len 함수를 사용하지 않습니다.
+def check_user_id(user):
+    # user.id의 길이가 범위를 만족하면 True를 반환
+    count = 0
+    for i in user['user_id']:
+        count += 1
+    if 4 <= count < 16:
+        return True
+    # 나머지는 False를 반환
+    return False
+    # 여기에 코드를 작성하여 함수를 완성합니다.
+    
+
+# 추가 테스트를 위한 코드 작성 가능
+# 예) print(함수명(인자))
+
+#####################################################
+# 아래 코드를 삭제하는 경우 
+# 모든 책임은 삭제한 본인에게 있습니다. 
+############## 테스트 코드 삭제 금지 #################
+user_data1 = {
+    'user_id': 'leessafy24',
+    'password': 'q1w2e3r4',
+    'password_confirm': 'q1w2e3r4',
+    'email': 'goodjob24@mail.com',
+}
+print(check_user_id(user_data1))  # True
+
+user_data2 = {
+    'user_id': 'edu',
+    'password': 'q1w2e3r4',
+    'password_confirm': 'asdf123',
+    'email': 'mail24.mail.com',
+}
+print(check_user_id(user_data2))  # False
+#####################################################
